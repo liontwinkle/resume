3005ceaa0d637b12f38a7025fad5565984f98e8f
lion <lion.twinkle.ge@gmail.com>
2021-04-09 23:34:51 +0200

    update cv

--- js/custom.js
+++ js/custom.js
@@ -103,9 +103,6 @@ $(document).ready(function () {
 
     });
 
-    //animatedModal
-    // $("#demo01,#demo02,#demo03,#demo04,#demo05,#demo06,#demo07,#demo08,#demo09").animatedModal();
-
     // Contact Form 	
 
     // validate contact form
